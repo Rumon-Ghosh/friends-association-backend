cef15ddea9f09c56d2371bd69271e4ae00ca771b
Rumon-Ghosh <rumon698@gmail.com>
2026-04-06 11:56:23 +0600

    added production environment

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -54,6 +54,7 @@ export const login = async (req: Request, res: Response): Promise<any> => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // Must be 'none' for cross-origin
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
